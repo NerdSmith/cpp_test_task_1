481dd428ee4ba01e0e9209c32cf30b1bce48bf63
Nikita Merzlyakov <merzlyakov02@gmail.com>
2022-04-05 21:58:16 +0300

    Добавьте файлы проекта.

---
+++ test_task_1/test_task_1_server.cpp
@@ -0,0 +1,273 @@
+﻿#include <ws2tcpip.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <fileapi.h>
+using namespace std;
+
+#pragma comment(lib, "Ws2_32.lib")
+
+
+#define DEFAULT_BUFLEN 512
+#define RESERVE_BLOCK_LENGTH 5
+#define MSG_LEN DEFAULT_BUFLEN - RESERVE_BLOCK_LENGTH
+
+
+int main(int argc, char* argv[])
+{
+	locale::global(locale());
+	if (argc < 4) {
+		printf("Not enough command line arguments, got: %d, need: 3", argc - 1);
+		return 1;
+	}
+
+	WSADATA wsaData;
+
+	char recvbuf[DEFAULT_BUFLEN];
+	int iResult, iSendResult;
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	SOCKET ListenSocket = INVALID_SOCKET;
+	SOCKET ClientSocket = INVALID_SOCKET;
+	SOCKET UDPClientSocket = INVALID_SOCKET;
+
+	struct addrinfo* result = NULL;
+	struct addrinfo TCPHints;
+	struct addrinfo UDPHints;
+
+	char* ipAddr = argv[1];
+	char* port = argv[2];
+
+	string dirName = argv[3];
+	if (CreateDirectoryA(dirName.c_str(), NULL) ||
+		ERROR_ALREADY_EXISTS == GetLastError())
+	{
+		printf("Dir is OK\n");
+	}
+	else
+	{
+		printf("Can't create dir\n");
+		return 1;
+	}
+
+	char udpPort[RESERVE_BLOCK_LENGTH];
+	char filenameBuf[MSG_LEN];
+	string filename;
+
+	struct sockaddr_in SenderAddr;
+	int SenderAddrSize = sizeof(SenderAddr);
+
+	fd_set rset;
+	FD_ZERO(&rset);
+
+	map<int, vector<char>> dataBlocks;
+	int blockNb;
+	char blockNbBuf[RESERVE_BLOCK_LENGTH];
+	char fileDataBuf[MSG_LEN];
+
+	string fileFullPath;
+	int i;
+	
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+	ZeroMemory(&TCPHints, sizeof(TCPHints));
+	TCPHints.ai_family = AF_INET;
+	TCPHints.ai_socktype = SOCK_STREAM;
+	TCPHints.ai_protocol = IPPROTO_TCP;
+
+	ZeroMemory(&UDPHints, sizeof(UDPHints));
+	UDPHints.ai_family = AF_INET;
+	UDPHints.ai_socktype = SOCK_DGRAM;
+	UDPHints.ai_protocol = IPPROTO_UDP;
+
+	iResult = getaddrinfo(ipAddr, port, &TCPHints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		printf("bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	freeaddrinfo(result);
+
+	iResult = listen(ListenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR) {
+		printf("listen failed with error: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	while (TRUE) {
+		ClientSocket = accept(ListenSocket, NULL, NULL);
+		if (ClientSocket == INVALID_SOCKET) {
+			printf("accept failed: %d\n", WSAGetLastError());
+			closesocket(ListenSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		do {
+			iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+			if (iResult > 0) {
+				printf("Bytes received: %d\n", iResult);
+				iSendResult = send(ClientSocket, recvbuf, iResult, 0);
+				if (iSendResult == SOCKET_ERROR) {
+					printf("send failed: %d\n", WSAGetLastError());
+					closesocket(ClientSocket);
+					WSACleanup();
+					return 1;
+				}
+				printf("Bytes sent: %d\n", iSendResult);
+				if (iResult == iSendResult) {
+					break;
+				}
+			}
+			else if (iResult == 0)
+				printf("Connection closing...\n");
+			else {
+				printf("recv failed: %d\n", WSAGetLastError());
+				closesocket(ClientSocket);
+				WSACleanup();
+				return 1;
+			}
+		} while (iResult > 0);
+
+		// udp_port & filename
+
+		memcpy(udpPort, recvbuf, RESERVE_BLOCK_LENGTH);
+		memcpy(filenameBuf, recvbuf + RESERVE_BLOCK_LENGTH, MSG_LEN);
+		filename = filenameBuf;
+		printf("UDP port: %s\n", udpPort);
+		printf("Filename: %s\n", filename.c_str());
+		memset(recvbuf, 0, sizeof(recvbuf));
+
+		iResult = getaddrinfo(ipAddr, udpPort, &UDPHints, &result);
+		if (iResult != 0) {
+			printf("getaddrinfo failed: %d\n", iResult);
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		// UDP
+
+		UDPClientSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+		if (UDPClientSocket == INVALID_SOCKET) {
+			printf("Socket failed with error %d\n", WSAGetLastError());
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+
+
+		iResult = bind(UDPClientSocket, result->ai_addr, (int)result->ai_addrlen);
+		if (iResult != 0) {
+			printf("Bind failed with error %d\n", WSAGetLastError());
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		memset(blockNbBuf, 0, sizeof(blockNbBuf));
+		memset(fileDataBuf, 0, sizeof(fileDataBuf));
+
+		printf("Receiving datagrams...\n");
+
+		for (;;) {
+			FD_SET(ClientSocket, &rset);
+			FD_SET(UDPClientSocket, &rset);
+
+			iResult = select(0, &rset, NULL, NULL, NULL);
+
+			if (FD_ISSET(UDPClientSocket, &rset)) {
+				iResult = recvfrom(UDPClientSocket,
+					recvbuf, DEFAULT_BUFLEN, 0, (SOCKADDR*)&SenderAddr, &SenderAddrSize);
+				if (iResult == SOCKET_ERROR) {
+					printf("Recvfrom failed with error %d\n", WSAGetLastError());
+				}
+				memcpy(blockNbBuf, recvbuf, RESERVE_BLOCK_LENGTH);
+				blockNb = atoi(blockNbBuf);
+				memcpy(fileDataBuf, recvbuf + RESERVE_BLOCK_LENGTH, DEFAULT_BUFLEN - (RESERVE_BLOCK_LENGTH + 1));
+				printf("Got block %d\n", blockNb);
+
+				vector<char> buffer(fileDataBuf, fileDataBuf + sizeof(fileDataBuf));
+				dataBlocks.emplace(blockNb, move(buffer));
+
+				iSendResult = send(ClientSocket, recvbuf, iResult, 0);
+				if (iSendResult == SOCKET_ERROR) {
+					printf("send failed: %d\n", WSAGetLastError());
+					continue;
+				}
+				printf("Bytes sent: %d\n", iSendResult);
+				if (iResult == iSendResult) {
+					continue;
+				}
+			}
+
+			if (FD_ISSET(ClientSocket, &rset)) {
+				iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+				if (iResult > 0) {
+					printf("Bytes received: %d\n", iResult);
+				}
+				else if (iResult == 0)
+					printf("Connection closing...\n");
+				else {
+					printf("recv failed: %d\n", WSAGetLastError());
+					closesocket(ClientSocket);
+					WSACleanup();
+					return 1;
+				}
+				break;
+			}
+		}
+
+		closesocket(UDPClientSocket);
+		printf("blocks nb: %d\n", dataBlocks.size());
+
+		fileFullPath = dirName + "\\" + filename;
+
+		ofstream myfile(fileFullPath);
+
+		for (i = 0; i < dataBlocks.size(); i++) {
+			printf("writing block: %d\n", i + 1);
+			myfile.write((const char*)&(dataBlocks[i])[0], (dataBlocks[i]).size());
+		}
+
+		myfile.close();
+		printf("Transmission done, file path: %s\n", fileFullPath.c_str());
+		memset(&recvbuf[0], 0, sizeof(recvbuf));
+		dataBlocks.clear();
+	}
+	
+	closesocket(ListenSocket);
+
+	closesocket(ClientSocket);
+	WSACleanup();
+
+	return 0;
+}
---
+++ test_task_1_client/test_task_1_client.cpp
@@ -0,0 +1,263 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <cstdio>
+#include <fstream>
+#include <set>
+#include <iostream>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define DEFAULT_BUFLEN 512
+#define RESERVE_BLOCK_LENGTH 5
+#define MSG_LEN DEFAULT_BUFLEN - RESERVE_BLOCK_LENGTH
+
+int main(int argc, char* argv[]) 
+{
+	if (argc < 4) {
+		printf("Not enough command line arguments, got: %d, need: 5", argc - 1);
+		return 1;
+	}
+
+	WSADATA wsaData;
+	int iResult;
+	int sendResult;
+	int receiveResult;
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	SOCKET UDPSocket = INVALID_SOCKET;
+
+	char* ipAddr = argv[1];
+	char* port = argv[2];
+	char* udpPort = argv[3];
+	char* filename = argv[4];
+	timeval timeout = timeval();
+	timeout.tv_sec = 0;
+	timeout.tv_usec = atoi(argv[5]);
+
+	int recvbuflen = DEFAULT_BUFLEN;
+	char recvbuf[DEFAULT_BUFLEN];
+
+	struct addrinfo* result = NULL,
+					* ptr = NULL,
+					TCPHints,
+					UDPHints;
+
+	char payload[DEFAULT_BUFLEN];
+
+	char fileData[DEFAULT_BUFLEN];
+	std::set<int> pNumbers;
+
+	int blockNb = 0;
+	char blockNbBuf[RESERVE_BLOCK_LENGTH + 1];
+	int recvBlockNb;
+	char recvBlockNbBuf[RESERVE_BLOCK_LENGTH];
+
+	int selectRes;
+
+	fd_set rset;
+	FD_ZERO(&rset);
+
+	// setup
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+
+	ZeroMemory(&TCPHints, sizeof(TCPHints));
+	TCPHints.ai_family = AF_INET;
+	TCPHints.ai_socktype = SOCK_STREAM;
+	TCPHints.ai_protocol = IPPROTO_TCP;
+
+
+	iResult = getaddrinfo(ipAddr, port, &TCPHints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+			ptr->ai_protocol);
+		if (ConnectSocket == INVALID_SOCKET) {
+			printf("socket failed with error: %ld\n", WSAGetLastError());
+			WSACleanup();
+			return 1;
+		}
+
+		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+		if (iResult == SOCKET_ERROR) {
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+			continue;
+		}
+		break;
+	}
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server!\n");
+		WSACleanup();
+		return 1;
+	}
+
+	// create port_filename payload
+
+	memset(&payload[0], 0, sizeof(payload));
+	memcpy(payload, udpPort, strlen(udpPort));
+	memcpy(payload + RESERVE_BLOCK_LENGTH, filename, strlen(filename));
+
+	//send port & filename
+	do {
+		sendResult = send(ConnectSocket, payload, (int)sizeof(payload), 0);
+		if (sendResult == SOCKET_ERROR) {
+			printf("send failed: %d\n", WSAGetLastError());
+			closesocket(ConnectSocket);
+			WSACleanup();
+			return 1;
+		}
+		printf("Bytes Sent: %ld\n", sendResult);
+
+		receiveResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+
+		if (receiveResult > 0)
+			printf("Bytes received: %d\n", receiveResult);
+		else if (receiveResult == 0)
+			printf("Connection closed\n");
+		else
+			printf("recv failed: %d\n", WSAGetLastError());
+
+	} while ((sendResult != receiveResult) || (receiveResult == 0));
+
+	// create upd socket
+	ZeroMemory(&UDPHints, sizeof(UDPHints));
+	UDPHints.ai_family = AF_INET;
+	UDPHints.ai_socktype = SOCK_DGRAM;
+	UDPHints.ai_protocol = IPPROTO_UDP;
+
+
+	iResult = getaddrinfo(ipAddr, udpPort, &UDPHints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	UDPSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (UDPSocket == INVALID_SOCKET) {
+		wprintf(L"socket failed with error %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	// send file data
+
+	memset(&blockNbBuf[0], 0, sizeof(blockNbBuf));
+
+	std::ifstream myfile (filename);
+	if (!myfile) {
+		printf("Unable to open file");
+		closesocket(UDPSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	memset(&fileData[0], 0, sizeof(fileData));
+
+	for (;;) {
+		_itoa_s(blockNb, blockNbBuf, 10);
+		printf("block nb %s\n", blockNbBuf);
+
+		memcpy(fileData, blockNbBuf, sizeof(blockNbBuf) - 1);
+
+		myfile.read(&fileData[RESERVE_BLOCK_LENGTH], MSG_LEN - 1);
+
+		while (TRUE) {
+			FD_ZERO(&rset);
+			FD_SET(ConnectSocket, &rset);
+
+			iResult = sendto(UDPSocket, fileData, sizeof(fileData),
+				0, result->ai_addr, result->ai_addrlen);
+			if (iResult == SOCKET_ERROR) {
+				printf("send failed: %d\n", WSAGetLastError());
+				closesocket(UDPSocket);
+				WSACleanup();
+				return 1;
+			}
+			pNumbers.insert(blockNb);
+			printf("Bytes Sent: %ld\n", iResult);
+		
+			selectRes = select(0, &rset, NULL, NULL, &timeout);
+			if (selectRes == SOCKET_ERROR) {
+				printf("select error: %d\n", WSAGetLastError());
+				closesocket(UDPSocket);
+				WSACleanup();
+			}
+			else if (selectRes == 0) {
+				printf("timeout\n");
+				continue;
+			}
+			else {
+				printf("recv %d\n", selectRes);
+				receiveResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+
+				if (receiveResult > 0) {
+					memcpy(recvBlockNbBuf, recvbuf, RESERVE_BLOCK_LENGTH);
+					recvBlockNb = atoi(recvBlockNbBuf);
+
+					printf("curr pnb %d | got %d\n", blockNb, recvBlockNb);
+
+					if (pNumbers.find(recvBlockNb) == pNumbers.end()) {
+						continue;
+					}
+					else {
+						pNumbers.erase(recvBlockNb);
+						printf("Bytes received: %d\n", receiveResult);
+					}
+				}
+				else if (receiveResult == 0)
+					printf("Connection closed\n");
+				else
+					printf("Recv failed: %d\n", WSAGetLastError());
+				break;
+			}
+		}
+		if (myfile.eof() && pNumbers.empty()) {
+			sendResult = send(ConnectSocket, NULL, 0, 0);
+			if (sendResult == SOCKET_ERROR) {
+				printf("send failed: %d\n", WSAGetLastError());
+				closesocket(ConnectSocket);
+				WSACleanup();
+				return 1;
+			}
+			printf("Bytes Sent: %ld\n", sendResult);
+			break;
+
+		}
+		else {
+			blockNb++;
+			memset(&fileData[0], 0, sizeof(fileData));
+			memset(&blockNbBuf[0], 0, sizeof(blockNbBuf));
+		}
+	}
+
+	myfile.close();
+
+	// end of transmission
+
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	closesocket(ConnectSocket);
+	WSACleanup();
+
+	return 0;
+}
